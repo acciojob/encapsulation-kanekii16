48c037c32ef2aeb4d78c30dc43719a14b72548b1
kanekii16 <iamsai1616@gmail.com>
2023-06-10 05:09:40 +0530

    Encapsulation in java Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly a = new RWOnly();
+
+        a.setName("Anuja");
+        a.getName();
+    }
   
 }
+
+/*
+* private members of class cannot be accessed outside class.
+* private member of class can be accessed only using getter and setter.
+* */
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void getName(){
+        System.out.println(this.name);
+    }
 }
